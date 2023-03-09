c0974f49eb72d0be5316a694e5fecc5a11bf4e85
dickslomibao <101649663+dickslomibao@users.noreply.github.com>
2023-03-09 09:25:05 +0800

    with core

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:contact_tracing/firebase_options.dart';
 import 'package:contact_tracing/view/screens/home.dart';
 import 'package:contact_tracing/view/screens/user_login.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
   runApp(const MyApp());
 }
 
